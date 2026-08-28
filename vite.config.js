cf9e17ebacc9aa41677a027af0aa0cffcbe62079
Farooq Muktar Aliyu <farooqmuktaraliyu@Farooqs-MacBook-Pro.local>
2026-08-28 11:36:51 +0100

    Fix Vercel Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
-    chunkSizeWarningLimit: 1000
-  }
+    chunkSizeWarningLimit: 1000,
+  },
 });
